b6034d00590f0f7e6debadee6f2c3225fcb16d7a
fast1sh <34493866+fast1sh@users.noreply.github.com>
2019-02-21 23:56:34 +0300

    Create tree.js

---
+++ tree.js
@@ -0,0 +1,31 @@
+const renderTree = (n) => {
+  const width = n + n - 1;
+
+  const render = (currentLvl, points, acc) => {
+    if (currentLvl === n) {
+      return acc;
+    }
+
+    const [ start, finish ] = points;
+    const hasToy = currentLvl % 2 === 0 ? true : false;
+    const toyPosition = start + Math.floor(Math.random() * (finish + 1 - start));
+    let newLine = ``;
+
+    for(let i = 0; i < width; i += 1) {
+
+      if (hasToy && i === toyPosition) {
+        newLine = `${newLine}o`;
+        continue;
+      }
+
+      const item = i < start || i > finish ? '_' : '*';
+      newLine = `${newLine}${item}`;
+    }
+
+    const newAcc = `${newLine}\n${acc}`;
+
+    return render(currentLvl + 1, [start + 1, finish - 1], newAcc);
+  }
+
+  return render(0, [0, width - 1], ``);
+}
